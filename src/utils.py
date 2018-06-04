4b3148cd8a12a83179288f2c1fddbbbe4675773d
Robert-Alonso <robert.alonso.ac@gmail.com>
2018-06-04 12:44:07 -0500

    Make validation set

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,33 @@
 import pandas as pd
 
 def apply_cats(df, trn):
-    """Changes any columns of strings in df into categorical variables using trn as
-    a template for the category codes."""
+    """Changes any columns of strings in df (DataFrame) into categorical variables
+    using trn (DataFrame) as a template for the category codes (inplace)."""
     for n,c in df.items():
         if (n in trn.columns) and (trn[n].dtype.name=='category'):
             df[n] = pd.Categorical(c, categories=trn[n].cat.categories, ordered=True)
+            
+def make_val_set(day_df, challenge):
+    """Makes a validation set from the information of the given day (DataFrame)
+    filling complementary BuySell labels and including the data points from the
+    original test set in challenge (DataFrame)"""
+    val = {}
+    date = day_df['TradeDateKey'].unique()[0]
+    for idx, row in challenge.iterrows():
+        val[(date, row['CustomerIdx'], 
+             row['IsinIdx'], row['BuySell'])] = 0
+    # 0s first, then 1s
+    for idx, row in day_df.sort_values('CustomerInterest').iterrows():
+        key = (date, row['CustomerIdx'], 
+               row['IsinIdx'], row['BuySell'])
+        val[key] = row['CustomerInterest']
+        key = list(key)
+        key[-1] = 'Sell' if row['BuySell'] == 'Buy' else 'Buy'
+        key = tuple(key)
+        if key not in val:
+            val[key] = 0
+    val = pd.DataFrame(pd.Series(val)).reset_index()
+    val.columns = ['TradeDateKey', 'CustomerIdx', 'IsinIdx', 'BuySell',
+                   'CustomerInterest']
+    return val
+    
